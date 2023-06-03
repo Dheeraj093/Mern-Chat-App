a1c3d0a7a146ae2d2f1c66ba8c1f61669cf0425d
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-06-03 22:44:41 +0530

    commit

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
                 },
             };
             const { data } = await axios.post(
-                "http://localhost:5000/api/user/login",
+                "https://chat-app-00vu.onrender.com/api/user/login",
                 { email, password },
                 config
             );
