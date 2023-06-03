c5382eac7cb70dd2c4ee1b42aabd1f8dc77008ae
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-06-03 22:19:51 +0530

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
-    // try {
-        return mongoose.connect("mongodb+srv://dheerajrajput1232:Dheeraj123@cluster0.2zwuols.mongodb.net/test?retryWrites=true&w=majority", {
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect("mongodb+srv://dheerajrajput1232:Dheeraj123@cluster0.2zwuols.mongodb.net/test?retryWrites=true&w=majority", {
             useNewUrlParser: true,
-            // useUnifiedTopology: true,
+            useUnifiedTopology: true,
         
         });
 
-        // console.log(`MongoDB Connected: ${conn.connection.host}`);
-        // return conn;
-    // } catch (error) {
-    //     console.log(`Error : ${error.message}`);
-    //     process.exit();
-    // }
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return conn;
+    } catch (error) {
+        console.log(`Error : ${error.message}`);
+        process.exit();
+    }
 }
 
 module.exports = connectDB;
