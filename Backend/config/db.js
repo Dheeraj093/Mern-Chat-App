faba066a29e798db63267c779c739131fe510857
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-06-03 16:42:04 +0530

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, {
+        const conn = await mongoose.connect("mongodb+srv://dheerajrajput1232:Dheeraj1234@cluster0.4ipinrx.mongodb.net/Chataap?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-
+        
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return conn;
     } catch (error) {
         console.log(`Error : ${error.message}`);
         process.exit();
