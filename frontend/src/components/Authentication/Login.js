e63794f6ba708d1898164d0414b1e1fe96d31143
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-08-23 09:27:07 +0530

    first commit

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
                 },
             };
             const { data } = await axios.post(
-                "api/user/login",
+                "/api/user/login",
                 { email, password },
                 config
             );
